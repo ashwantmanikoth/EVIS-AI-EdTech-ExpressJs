e0da2594c7405d14ebf7cf56006a908c66a0a6e1
ashwantmanikoth <ashwanta75@gmail.com>
2024-04-08 19:45:31 -0300

    fix(app): prompt changes

--- routes/AWSfunctions.js
+++ routes/AWSfunctions.js
@@ -237,7 +237,7 @@ async function callOpenAi(keywordsJson) {
         { role: "system", content: "You are a Quiz Generating assistant." },
         {
           role: "user",
-          content: "Generate five quiz questions based on the provided keywords. Each question should have four options, with one correct answer, and make sure that there are two distinct topics only. Return the questions in JSON format with the following structure: [{'question': 'Example question?', 'options': ['Option 1', 'Option 2', 'Option 3', 'Option 4'], 'correct_option': 0, 'topic': 'Example topic'}]. Here are the keywords: " + keywordsJson
+          content: "Generate five quiz questions based on the provided keywords. Each question should have four options, with one correct answer, and make sure that all questions come from 2 related topics. Return the questions in JSON format with the following structure: [{'question': 'Example question?', 'options': ['Option 1', 'Option 2', 'Option 3', 'Option 4'], 'correct_option': 0, 'topic': 'topic1'}]. Here are the keywords: " + keywordsJson
         },
       ],
       model: "gpt-3.5-turbo",
